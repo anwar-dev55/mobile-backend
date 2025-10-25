50d2bf3f2ae99e451ea3dcebe1543b45bd01c475
anwar-dev55 <anwarmaro681@gmail.com>
2025-10-26 01:55:28 +0300

    update something

--- DB/db.js
+++ DB/db.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.DATABASE,
+  ssl: process.env.DATABASE.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false },
 });
 
-pool.connect()
-  .then(() => console.log("✅ Connected to PostgreSQL successfully"))
-  .catch(err => console.error("❌ DB connection error:", err));
-
-module.exports = pool;
+module.exports = pool;
