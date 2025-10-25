f33c26e582600cb726b5971e29dd471c96a05402
anwar-dev55 <anwarmaro681@gmail.com>
2025-10-26 02:15:06 +0300

    update somthing

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+console.log("userRoutes loaded successfully")
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
