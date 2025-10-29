7c02b1953a56cc1af68a3a75031cf75fbf22a4b5
anwar-dev55 <anwarmaro681@gmail.com>
2025-10-29 12:34:03 +0300

    fix: something

--- router/user.router.js
+++ router/user.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+console.log("user.router.js is loaded");
 const usercontroller = require('../controller/user.controller');
 
 router.post('/register', usercontroller.register);
 router.post('/login', usercontroller.login);
 
-module.exports = router;
+module.exports = router;
