98ac696e93f9c34e55166b73c46d1452b5e2c989
anwar-dev55 <anwarmaro681@gmail.com>
2025-10-29 13:23:24 +0300

    fix: something

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 
